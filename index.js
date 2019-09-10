122b3aac866961a3f43cff94a17abf66c4065990
Natalie <natsetc@gmail.com>
2019-09-09 18:40:47 -0700

    notes

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ class Deciduous extends Tree {
     static definition() {
         return (
             super.definition() + " Deciduous trees shed their leaves annually."
+
+            // or 
+            // return `${super.definition()} Deciduous trees shed their leaves annually.`
         );
     }
 }
